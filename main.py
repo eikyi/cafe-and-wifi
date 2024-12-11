e4692cfb9e7e710d15cd6f01d0484c2c10e1b35f
eikyi <eikyi27@gmail.com>
2024-12-11 21:03:15 +1100

    Added cafe details page and Report close function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 import requests
 from dotenv import load_dotenv
@@ -9,11 +9,13 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 BASE_URL = os.environ.get('CAFE_API_BASE_URL')
+DELETE_API_KEY = os.environ.get('DELETE_API_KEY')
 Bootstrap5(app)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
 @app.route("/list")
 def get_all_cafe():
     #reqest all cafe data
@@ -23,9 +25,18 @@ def get_all_cafe():
 
     return render_template('cafe-list.html',cafes = cafes)
 
-@app.route("/details")
-def cafe_details():
-    return render_template('cafe-details.html')
+@app.route("/details/<cafe_id>")
+def cafe_details(cafe_id):
+    response_cafe = requests.get(f"{BASE_URL}search?id={cafe_id}")
+    response_cafe.raise_for_status()
+    cafe = response_cafe.json()['cafes'][0]
+    return render_template('cafe-details.html',cafe=cafe)
+
+@app.route("/close/<cafe_id>")
+def close_cafe(cafe_id):
+    response = requests.delete(f"{BASE_URL}report-closed/{cafe_id}?api-key={DELETE_API_KEY}")
+    response.raise_for_status()
+    return redirect(url_for('get_all_cafe'))
 
 if __name__ == "__main__":
     app.run(debug=False)
